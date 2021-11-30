d49ef5ec9c8a6dfa1095395605c55454ebc13d9d
Karol <kariurek@gmail.com>
2021-11-30 12:45:47 +0100

    header style

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 
 var image = document.getElementsByClassName('bg-stars');
 new simpleParallax(image, {
-	delay: 1.6,
+	delay: 0.0,
+    scale: 1.1,
 	transition: 'cubic-bezier(0,0,0,1)'
+});
+
+var mclouds = document.getElementsByClassName('main-clouds');
+new simpleParallax(mclouds, {
+	delay: 0.0,
+    scale: 2.2,
+	transition: 'cubic-bezier(0,0,0,1)',
+});
+
+var mbirds = document.getElementsByClassName('h-birds');
+new simpleParallax(mbirds, {
+	delay: 0.0,
+    scale: 2.0,
+	transition: 'cubic-bezier(0,0,0,1)',
 });
